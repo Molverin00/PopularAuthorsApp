a41028f6a1b4f50b3164b9f258f9927a76ee2472
Marouen CHAOUCH <chaouch.marouen@gmail.com>
2023-08-02 13:26:31 +0100

    docs(model): Added docs to Author class (model/dto/entity)

--- app/src/main/java/com/example/popularauthorsapp/data/model/Author.kt
+++ app/src/main/java/com/example/popularauthorsapp/data/model/Author.kt
@@ -1,16 +1,31 @@
 package com.example.popularauthorsapp.data.model
 
-import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 
+/**
+ * Unified representation of an Author, serving as a combination of DTO, model, and entity classes.
+ * This data class is used for simplification purposes to avoid redundant data classes and mapping.
+ *
+ * @param authorId The unique identifier of the author.
+ * @param name The name of the author.
+ * @param image The URL of the author's image.
+ * @param url The URL of the author's profile.
+ * @param popularBookTitle The title of the author's popular book.
+ * @param popularBookUrl The URL of the author's popular book.
+ * @param numberPublishedBooks The number of books published by the author.
+ */
 @Entity(tableName = "authors")
 data class Author(
+    @PrimaryKey
     @SerializedName("author_id")
-    @PrimaryKey val authorId: Long,
+    val authorId: Long,
+    @SerializedName("name")
     val name: String,
+    @SerializedName("image")
     val image: String,
+    @SerializedName("url")
     val url: String,
     @SerializedName("popular_book_title")
     val popularBookTitle: String,
