a6e556fb13d78ed4b31b93d9bbb15968fbe0b1ce
Marouen CHAOUCH <chaouch.marouen@gmail.com>
2023-08-02 16:29:38 +0100

    feat(repository): Implemented networkBoundResource function and inject repository dependencies

--- app/src/main/java/com/example/popularauthorsapp/data/AuthorRepository.kt
+++ app/src/main/java/com/example/popularauthorsapp/data/AuthorRepository.kt
@@ -1,18 +1,36 @@
 package com.example.popularauthorsapp.data
 
-import com.example.popularauthorsapp.data.local.AuthorDao
-import com.example.popularauthorsapp.data.model.Author
+import com.example.popularauthorsapp.core.util.networkBoundResource
+import com.example.popularauthorsapp.data.local.AppDatabase
 import com.example.popularauthorsapp.data.remote.AuthorsApiService
+import kotlinx.coroutines.delay
+import javax.inject.Inject
 
-class AuthorRepository(private val apiService: AuthorsApiService, private val authorDao: AuthorDao) {
-    suspend fun getTopAuthors(): List<Author> {
-        val cachedAuthors = authorDao.getAllAuthors()
-        return if (cachedAuthors.isNotEmpty()) {
-            cachedAuthors
-        } else {
-            val authors = apiService.getTopAuthors()
+class AuthorRepository @Inject constructor(
+    private val apiService: AuthorsApiService,
+    private val database: AppDatabase
+) {
+
+    private val authorDao = database.authorDao()
+
+    fun getTopAuthors() = networkBoundResource(
+        query = {
+            // Fetch authors from the local database
+            authorDao.getAllAuthors()
+        },
+        fetch = {
+            // Fetch authors from the remote API
+            delay(2000) // delay result just to test loading state
+            apiService.getTopAuthors()
+        },
+        saveFetchResult = { authors ->
+            // Replace old data with the fetched authors in the local database
+            authorDao.deleteAllAuthors()
             authorDao.insertAllAuthors(authors)
-            authors
+        },
+        shouldFetch = { data ->
+            // Perform a network request only if the local database is empty
+            data.isEmpty() // TODO: Change this condition to change caching strategy
         }
-    }
+    )
 }
