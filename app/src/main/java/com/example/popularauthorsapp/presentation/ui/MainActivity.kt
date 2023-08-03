9a6af74a1eef5a52b0bcafeca24018d7df5572dd
Marouen CHAOUCH <chaouch.marouen@gmail.com>
2023-08-03 11:18:54 +0100

    feat(viewModel): Implemented force / manual fetch function in viewModel and added swipeRefresh in activity

--- app/src/main/java/com/example/popularauthorsapp/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/example/popularauthorsapp/presentation/ui/MainActivity.kt
@@ -3,42 +3,120 @@ package com.example.popularauthorsapp.presentation.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.core.view.isVisible
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.recyclerview.widget.LinearLayoutManager
-//import com.example.popularauthorsapp.data.remote.RetrofitClient.apiService
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import com.example.popularauthorsapp.R
+import com.example.popularauthorsapp.util.Resource
 import com.example.popularauthorsapp.databinding.ActivityMainBinding
 import com.example.popularauthorsapp.presentation.adapters.AuthorsAdapter
 import com.example.popularauthorsapp.presentation.viewModels.AuthorViewModel
+import com.example.popularauthorsapp.util.ConnectivityMonitor
+import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private val authorViewModel: AuthorViewModel by viewModels()
+    private lateinit var binding: ActivityMainBinding
     private lateinit var authorsAdapter: AuthorsAdapter
+    private lateinit var snackBar: Snackbar
+    private lateinit var swipeRefreshLayout: SwipeRefreshLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // Initialize the RecyclerView and its adapter
+
+        // Init views
+        initAuthorsRecyclerview()
+        initNetworkStatusSnackBar()
+
+        swipeRefreshLayout = binding.swipeRefreshLayout
+        swipeRefreshLayout.setOnRefreshListener {
+            // Trigger the refresh action here
+            lifecycleScope.launch {
+                repeatOnLifecycle(Lifecycle.State.STARTED) {
+                    authorViewModel.forceFetchTopAuthors()
+                    swipeRefreshLayout.isRefreshing = false
+                }
+            }
+
+        }
+
+
+        // Observe network status flow
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                authorViewModel.networkStatus.collect { status ->
+                    println("isConnected: ${status == ConnectivityMonitor.Status.Available}")
+                    showNetworkStatusSnackBar(status == ConnectivityMonitor.Status.Available)
+
+                    if (status == ConnectivityMonitor.Status.Available) {
+                        authorViewModel.fetchTopAuthorsManually()
+                    }
+                }
+            }
+        }
+
+
+        // Observe authors flow
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                authorViewModel.authors.collect() { result ->
+
+                    if (result.data != null) {
+                        println("result.data.size: ${result.data.size}")
+                        authorsAdapter.submitList(result.data)
+                    } else {
+                        println("result.data is null")
+                    }
+
+                    binding.pbLoadingProgressBar.isVisible =
+                        result is Resource.Loading && result.data.isNullOrEmpty()
+                    binding.tvErrorMessage.isVisible =
+                        result is Resource.Error && result.data.isNullOrEmpty()
+                    binding.tvErrorMessage.text = result.error?.localizedMessage
+
+                    swipeRefreshLayout.isRefreshing = false
+                }
+
+            }
+        }
+
+    }
+
+
+    private fun initAuthorsRecyclerview() {
         authorsAdapter = AuthorsAdapter()
         binding.recyclerView.apply {
             layoutManager = LinearLayoutManager(this@MainActivity)
             adapter = authorsAdapter
         }
+    }
 
-        authorViewModel.authors.observe(this@MainActivity) {authors ->
-            authorsAdapter.submitList(authors)
+
+    private fun initNetworkStatusSnackBar() {
+        snackBar = Snackbar.make(
+            binding.root,
+            getString(R.string.no_internet_connection), Snackbar.LENGTH_INDEFINITE
+        ).setAction(R.string.btn_label_dismiss) {
+            snackBar.dismiss()
         }
+    }
 
-        // Observe the LiveData to update the UI when data changes
-//        authorViewModel.authors.observe(this@MainActivity, Observer { result ->
-//            if (result.data == null) {
-//                println("result is empty")
-//            } else {
-//                authorsAdapter.submitList(result.data)
-//            }
-//        })
+
+    private fun showNetworkStatusSnackBar(isConnected: Boolean) {
+        if (!isConnected) {
+            snackBar.show()
+        } else {
+            snackBar.dismiss()
+        }
     }
 }
--- app/src/main/java/com/example/popularauthorsapp/presentation/viewModels/AuthorViewModel.kt
+++ app/src/main/java/com/example/popularauthorsapp/presentation/viewModels/AuthorViewModel.kt
@@ -1,75 +1,56 @@
 package com.example.popularauthorsapp.presentation.viewModels
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
+import com.example.popularauthorsapp.data.AuthorRepository
 import com.example.popularauthorsapp.data.model.Author
-import com.example.popularauthorsapp.data.remote.AuthorsApiService
+import com.example.popularauthorsapp.util.ConnectivityMonitor
+import com.example.popularauthorsapp.util.NetworkConnectivityMonitor
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 @HiltViewModel
-class AuthorViewModel @Inject constructor(apiService: AuthorsApiService) : ViewModel() {
+class AuthorViewModel @Inject constructor(
+    private val repository: AuthorRepository,
+    networkConnectivityMonitor: NetworkConnectivityMonitor
+) : ViewModel() {
 
-    private val authorsLiveData = MutableLiveData<List<Author>>()
-    val authors: LiveData<List<Author>> = authorsLiveData
+    /**
+     * A flow representing the network connectivity status observed using [networkConnectivityMonitor].
+     * It emits [ConnectivityMonitor.Status] values indicating the availability of the internet connection.
+     */
+    val networkStatus: Flow<ConnectivityMonitor.Status> = networkConnectivityMonitor.observe()
 
-    init {
-        viewModelScope.launch {
-            val authors = apiService.getTopAuthors()
-            delay(2000)
-            authorsLiveData.value = authors
-        }
+
+    /**
+     * A flow representing the top authors fetched from the [repository].
+     * It emits [Resource] with [List] of [Author] data, indicating the status of the fetch operation.
+     * Use this flow to observe and receive updated author data in real-time.
+     */
+    val authors = repository.getTopAuthors()
+
+
+    /**
+     * A suspend function used to force-fetch the top authors from the [repository].
+     * This function initiates a network request regardless of the current cached data and fetches the latest authors.
+     * Use this function when you want to ensure that the latest data is fetched from the remote API.
+     *
+     * @return A [List] of [Author] representing the latest authors fetched from the API.
+     */
+    suspend fun forceFetchTopAuthors(): List<Author> {
+        return repository.forceFetchTopAuthors()
     }
 
-//    val authors = repository.getTopAuthors().asLiveData()
 
-//    private val repository: AuthorRepository
-//    val topAuthorsLiveData: MutableLiveData<List<Author>> = MutableLiveData()
-//    val errorLiveData: MutableLiveData<Boolean> = MutableLiveData()
-//    val networkStatusLiveData: MutableLiveData<Boolean> = MutableLiveData()
-//    val connectedLiveData: MutableLiveData<Boolean> = MutableLiveData()
-//
-//    private var isConnected = false // To track network status
-//
-//    init {
-//        val apiService = RetrofitClient.apiService
-//        val authorDao = AppDatabase.getInstance(application).authorDao()
-//        repository = AuthorRepository(apiService, authorDao)
-//        fetchTopAuthors()
-//
-//        // Observe network changes
-//        val connectivityManager =
-//            application.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-//        connectivityManager.registerDefaultNetworkCallback(object : ConnectivityManager.NetworkCallback() {
-//            override fun onAvailable(network: Network) {
-//                isConnected = true
-//                networkStatusLiveData.postValue(true)
-//            }
-//
-//            override fun onLost(network: Network) {
-//                isConnected = false
-//                networkStatusLiveData.postValue(false)
-//            }
-//        })
-//    }
-//
-//    fun fetchTopAuthors() {
-//        viewModelScope.launch {
-//            try {
-//                val authors = repository.getTopAuthors()
-//                authors.collect()
-////                topAuthorsLiveData.postValue(authors)
-//                if (!isConnected) {
-//                    connectedLiveData.postValue(true) // Notify the MainActivity about the successful connection
-//                }
-//            } catch (e: Exception) {
-//                // Handle error here
-//                errorLiveData.postValue(true)
-//            }
-//        }
-//    }
+    /**
+     * A suspend function used to manually fetch the top authors from the [repository].
+     * This function first tries to fetch authors from the local database (cached data),
+     * and if it is empty, it initiates a network request to fetch authors from the remote API.
+     * Use this function to avoid unnecessary network requests if the data is already available in the local database.
+     *
+     * @return A [List] of [Author] representing the fetched authors from either the local database or the remote API.
+     */
+    suspend fun fetchTopAuthorsManually(): List<Author> {
+        return repository.fetchTopAuthorsManually()
+    }
 }
